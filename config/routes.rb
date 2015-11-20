214fc5dc2e448a58e069971979e2c1e97bbb9361
gurumohanreddy <gurumohan449@gmail.com>
2015-11-20 10:52:33 +0530

    Add pages for post resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  # get 'home/index'
+  # get 'posts/new'
+  #
+  # get 'posts/index'
 
+  # get 'home/index'
+  resources:posts
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'home#land'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
